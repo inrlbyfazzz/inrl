77bd916db4be2a30ae50b9fa4fab1b23a4bae018
fasweeh <105339604+fasweeh-fazzz@users.noreply.github.com>
2022-05-13 07:10:32 +0530

    fasweeh

--- qr.js
+++ qr.js
@@ -1,27 +1,24 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 */
 
 const chalk = require('chalk');
 const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
-const {StringSession} = require('./inrl/');
+const {StringSession} = require('./faz/');
 const fs = require('fs');
 
 async function whatsAsena () {
     const conn = new WAConnection();
     const Session = new StringSession();  
-    conn.version = [3, 3234,9]
+    conn.version = [2, 2119, 6]
     conn.logger.level = 'warn';
     conn.regenerateQRIntervalMs = 50000;
     
     conn.on('connecting', async () => {
         console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
 ${chalk.white.italic('AsenaString Kodu Alıcı')}
-
 ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
     });
     
@@ -33,7 +30,7 @@ ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
         );
         
         if (!fs.existsSync('config.env')) {
-            fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
+            fs.writeFileSync('config.env', `INRL_SESSION="${st}"`);
         }
         if (conn.user.jid.startsWith('90')) {
             await conn.sendMessage(conn.user.jid,st, MessageType.text)
